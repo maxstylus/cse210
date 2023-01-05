628d7adf5bc0d472be97525f94a32b7dc0908403
Lauren Snyder <lauren@topwebworks.com>
2023-01-04 21:47:25 -0700

    Finished all prep 1 C# assignments

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Enter a list of numbers, type 0 when finished.
+        // Enter number: 18
+        // Enter number: 36
+        // Enter number: 2
+        // Enter number: 48
+        // Enter number: 6
+        // Enter number: 12
+        // Enter number: 9
+        // Enter number: 0
+        // The sum is: 131
+        // The average is: 18.714285714285715
+        // The largest number is: 48
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Create the list
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Compute the Sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Compute the Average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the max 
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+
+
+
+  
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // DisplayWelcome - Displays the message, "Welcome to the Program!"
+        // PromptUserName - Asks for and returns the user's name (as a string)
+        // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+        // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+        // DisplayResult - Accepts the user's name and the squared number and displays them.
+
+        // Welcome to the program!
+        // Please enter your name: Brother Burton
+        // Please enter your favorite number: 42
+        // Brother Burton, the square of your number is 1764
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favorite_number = PromptUserFavoriteNumber();
+        int square = SquareNumber(favorite_number);
+        DisplayResults(name, square);
+
+    }
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserFavoriteNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResults(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square.ToString()}");
     }
 }
